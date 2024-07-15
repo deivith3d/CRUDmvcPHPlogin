2c9562ab026bcfab04a89dcffe092ff0b54614b8
Deivith Gonçalves da Cunha <dgcunha@senacinformatica.br>
2024-07-15 12:13:18 -0300

    Quase lá

---
+++ login/login.php
@@ -0,0 +1,48 @@
+<?php
+
+session_start();
+include_once '../dao/Conexao.php';
+$conex = new Conexao();
+$conex->fazConexao();
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $stmt = $conex->conn->prepare("SELECT * FROM users WHERE username = ?");
+    $stmt->execute([$username]);
+    $user = $stmt->fetch();
+    if ($user && password_verify($password, $user['password'])) {
+        $_SESSION['user_id'] = $user['id'];
+        $_SESSION['username'] = $user['username'];
+            include_once '../index.php';
+        exit();
+    } else {
+        $error = 'Nome de usuário ou senha inválidos';
+    }
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <h2>Login</h2>
+    <?php if (isset($error)): ?>
+        <p style="color:red;"><?php echo $error; ?></p>
+    <?php endif; ?>
+    <form method="POST">
+        <label>Nome de usuário:</label>
+        <input type="text" name="username" required>
+        <br>
+        <label>Senha:</label>
+        <input type="password" name="password" required>
+        <br>
+        <button type="submit">Entrar</button>
+        <br>
+        <a href="register.php">REGISTRE-SE. Crie uma nova conta.</a>
+    </form>
+</body>
+</html>
---
+++ view/formListarAluno.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário Listar Alunos</title>
+</head>
+<body>
+    <?php
+
+        if (!isset($_SESSION['user_id'])) {
+            header('Location: ../login/login.php');
+            exit();
+        }
+
+        include("../controller/AlunoController.php"); 
+        $res = AlunoController::listarAlunos();
+        $qtd = $res->rowCount();
+        if($qtd>0){
+            print"<table class='table table-hover table-striped table-bordered'>";
+            print "<tr>";
+            print "<th>#</th>";
+            print "<th>Nome</th>"; 
+            print "<th>Matrícula</th>";
+            print "<th>CPF</th>";
+            print "<th>Idade</th>";
+            print "<th>Email</th>";
+            print"</tr>";
+            while ($row = $res->fetch(PDO::FETCH_OBJ)) {
+                print "<tr>";
+                print "<td>".$row->idAlu."</td>";
+                print "<td>".$row->nomeAlu."</td>"; 
+                print "<td>".$row->matriculaAlu."</td>";
+                print "<td>".$row->cpfAlu."</td>";
+                print "<td>".$row->idadeAlu."</td>";
+                print "<td>".$row->emailAlu."</td>";
+                print "<td>
+                            <button onclick=\"location.href = '../view/formAluno.php?op=Alterar&idAluno=".$row->idAlu."';\">Alterar</button>
+                            <button onclick=\"if(confirm('Tem certeza que deseja excluir?')) {
+                                location.href = '../controller/processaAluno.php?op=Excluir&idAluno=".$row->idAlu."'; }
+                                else { false; }\">Excluir</button>
+                        </td>";
+                echo "</form>";
+                print"</tr>";
+            }
+            print"</table>";
+        }
+        else{
+            echo "<p>Nenhum registro encontrado!</p>";
+        }
+    ?>
+</body>
+</html>
